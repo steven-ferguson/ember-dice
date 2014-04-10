0b399c15d2fa340bae61ed15aea6471045941c5f
Steven Ferguson <steven.ferguson11@gmail.com>
2014-04-10 10:11:39 -0700

    Add dice component
    - Dice component for displaying a collection of dice
    - Move html from application template to index template
    - Add mock data to index route
    - Die component adds class for the die's value
    - Remove ember app kit sample tests and templates

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,5 @@
 export default Ember.Route.extend({
   model: function() {
-    return ['red', 'yellow', 'blue'];
+    return [{id: 1, value: 'one'}, {id: 2, value: 'two'}, {id: 3, value: 'three'}];
   }
 });
