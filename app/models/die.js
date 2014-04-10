e0f2ec7dad85645699e85d0d25d6d43a7dbea1a2
Steven Ferguson <steven.ferguson11@gmail.com>
2014-04-10 10:49:19 -0700

    Add textValue computed property to die
    The value of a die is now an integer, so a method was created
    to convert this value into a word that could be used for assigning
    the correct css class to a die.

--- app/models/die.js
+++ app/models/die.js
@@ -1,3 +1,9 @@
 export default Ember.Object.extend({
-  value: 'one'
+  value: 5,
+
+  textValue: function() {
+    var value = this.get('value');
+    var dictionary = { 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six'};
+    return dictionary[value];
+  }.property('value')
 });
