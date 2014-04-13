47b577b12bce0652f5c7544b9f28a1e68a181ab4
steven-ferguson <steven.ferguson11@gmail.com>
2014-04-13 12:49:17 -0700

    Reroll dice only twice
    Dice can be rerolled up to two times then
    the roll button is disabled.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,10 +1,19 @@
 export default Ember.ArrayController.extend({
+  remainingRolls: 2,
+
+  anyReRolls: function() {
+    return this.get('remainingRolls') == 0;
+  }.property('remainingRolls'),
+
   actions: {
     roll: function() {
       var selectedDice = this.filterBy('isSelected', true);
       selectedDice.forEach(function(die) {
         die.roll();
       });
+
+      var remainingRolls = this.get('remainingRolls');
+      this.set('remainingRolls', remainingRolls - 1);
     }
   }
 });
