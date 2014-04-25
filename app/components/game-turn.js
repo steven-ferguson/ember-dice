fd28a3ddd49111097c2cbe8a0e36b511c99178fe
steven-ferguson <steven.ferguson11@gmail.com>
2014-04-24 22:47:53 -0700

    Move turn buttons and actions into game-turn

--- app/components/game-turn.js
+++ app/components/game-turn.js
@@ -1,10 +1,36 @@
 import Die from "appkit/models/die";
 
 export default Ember.Component.extend({
+  score: 0,
+
+  selectedDice: function() {
+    return this.get('dice').filterBy('isSelected');
+  }.property('dice.@each.isSelected'),
+
   actions: {
-    updateScore: function(score) {
-      var turn = this.get('turn');
-      turn.set('score', score);
+    roll: function() {
+      var dice = this.get('selectedDice');
+      dice.forEach(function(die) {
+        die.roll();
+      });
+    },
+
+    scoreDice: function() {
+      var score;
+      score = this.get('selectedDice').reduce(function(previousValue, die) {
+        return previousValue + die.value;
+      }, 0);
+      this.set('score', score);
+      this.updateScore(score);
+    },
+
+    endTurn: function() {
+      this.sendAction('action');
     }
+  },
+
+  updateScore: function(score) {
+    var turn = this.get('turn');
+    turn.set('score', score);
   }
 });
